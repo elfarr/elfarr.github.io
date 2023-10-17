f838acbe018ea2fb7108e08630e73bf5e8ea0b18
greta novak <114183568+elfarr@users.noreply.github.com>
2023-10-17 18:04:20 +0300

    task6

--- task6/script.js
+++ task6/script.js
@@ -1,43 +1,78 @@
 window.addEventListener("DOMContentLoaded", function () {
+
   console.log("DOM fully loaded and parsed");
-  let f = document.getElementsByName("field");
+  let f = document.getElementById("field");
   let r = document.getElementById("result");
-  let s = document.getElementsByName("select1");
-  let btn = document.getElementById("button1");
+  const form = document.getElementById("form");
   let radio = document.getElementById("myradios");
+  let checkbox = document.getElementById("checkbox");
+  let s = document.getElementById("select1");
+  const cup = document.getElementById("cup");
+  const pencil = document.getElementById("pencil");
+  const pen = document.getElementById("pen");
+  const box = document.getElementById("box");
   price = {
     pen: 40,
     cup: 100, 
     pencil: 30,
-    cup1: 50,
-    cup2: 100,
-    cup3: 150,
+    shar: 10,
+    gel: 20,
+    erase: 100,
+    meh: 200,
+  };
+  
+  s.hidden = true;
+    checkbox.hidden = true;
+  radio.addEventListener("change", changeVisible);
+  let product = "cup";
+  function changeVisible() {
+   if (pen.checked) {
+    
+    product = "pen";
+    s.hidden = false;
+    checkbox.hidden = true;
+
+   }
+   if (cup.checked) {
+    product = "cup";
+    s.hidden = true;
+    checkbox.hidden = true;
+   }
+   if (pencil.checked) {
+    product = "pencil";
+    s.hidden = true;
+    checkbox.hidden = false;
+   }
+
   }
-  btn.onclick = function calculatePrice() {
-     let valid = /^[0-9]\d*$/.test(f[0].value);
-    const quantity = parseInt(f[0].value);
+function calculate() {
+     let valid = /^[0-9]\d*$/.test(f.value);
+    const quantity = parseInt(f.value);
+    total = quantity*price[product];
     if (!valid) {
       alert("Значение некорректно!");
       r.innerHTML = "";
-      f[-1].value = "";
-      s.value = "";
       return false;
     }
-    let total = quantity * price[product];
-    let product = radio.querySelector("input:checked").value;
-     if (product === "cup") {
-      s.style.display = "none";
-      radio.style.display = "none";
-    } else if (product === "pen") {
-      s.style.display = "block";
-      checkboxElement.style.display = "none";
-      totalCost += price[s.value] * quantity;
-    } else if (selectedProduct === "pencil") {
-      s.style.display = "none";
-      checkboxElement.style.display = "block"; };
+    if (product === 'pen') {
+    if (s.value === 'shar') {
+
+      total += quantity*price["shar"];
+    }
+    else if (s.value === 'gel') {
+      total += quantity*price["gel"];
+  }
+  else if (s.value === 'erase') {
+    total += quantity*price["erase"];
+}}
+else if (product === 'pencil' && box.checked) {
+  console.log("+");
+  total = price["meh"]*quantity;}
+    r.innerHTML = "<p>Стоимость: " + total +  " руб</p>";
     
-   
-    r.innerHTML = "<p>Стоимость: " +  + " руб</p>";
     return false;
-};
-});
+}
+  form.addEventListener("change", calculate);
+
+}
+);
