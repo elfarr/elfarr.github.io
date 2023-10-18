cb26096c8501c68a8fd1acdeec6d2be49e315abd
greta novak <114183568+elfarr@users.noreply.github.com>
2023-10-18 18:28:55 +0300

    valid

--- task6/script.js
+++ task6/script.js
@@ -11,68 +11,52 @@ window.addEventListener("DOMContentLoaded", function () {
   const pencil = document.getElementById("pencil");
   const pen = document.getElementById("pen");
   const box = document.getElementById("box");
-  price = {
-    pen: 40,
-    cup: 100, 
-    pencil: 30,
-    shar: 10,
-    gel: 20,
+  const price = {
+    cup: 100,
     erase: 100,
+    gel: 20,
     meh: 200,
+    pen: 40,
+    pencil: 30,
+    shar: 10
   };
-  
   s.hidden = true;
-    checkbox.hidden = true;
+  checkbox.hidden = true;
   radio.addEventListener("change", changeVisible);
   let product = "cup";
   function changeVisible() {
-   if (pen.checked) {
-    
-    product = "pen";
-    s.hidden = false;
-    checkbox.hidden = true;
-
-   }
-   if (cup.checked) {
-    product = "cup";
-    s.hidden = true;
-    checkbox.hidden = true;
-   }
-   if (pencil.checked) {
-    product = "pencil";
-    s.hidden = true;
-    checkbox.hidden = false;
-   }
-
+    if (pen.checked) {
+      product = "pen";
+      s.hidden = false;
+      checkbox.hidden = true;
+    }
+    if (cup.checked) {
+      product = "cup";
+      s.hidden = true;
+      checkbox.hidden = true;
+    }
+    if (pencil.checked) {
+      product = "pencil";
+      s.hidden = true;
+      checkbox.hidden = false;
+    }
   }
-function calculate() {
-     let valid = /^[0-9]\d*$/.test(f.value);
+  function calculate() {
+    let valid = /^[0-9]\d*$/.test(f.value);
     const quantity = parseInt(f.value);
-    total = quantity*price[product];
+    let total = quantity * price[product];
     if (!valid) {
       alert("Значение некорректно!");
       r.innerHTML = "";
       return false;
     }
-    if (product === 'pen') {
-    if (s.value === 'shar') {
-
-      total += quantity*price["shar"];
+    if (product === "pen") {
+      total += quantity * price[s.value];
+    } else if (product === "pencil" && box.checked) {
+      total = price.meh * quantity;
     }
-    else if (s.value === 'gel') {
-      total += quantity*price["gel"];
-  }
-  else if (s.value === 'erase') {
-    total += quantity*price["erase"];
-}}
-else if (product === 'pencil' && box.checked) {
-  console.log("+");
-  total = price["meh"]*quantity;}
-    r.innerHTML = "<p>Стоимость: " + total +  " руб</p>";
-    
+    r.innerHTML = "<p>Стоимость: " + total + " руб</p>";
     return false;
-}
+  }
   form.addEventListener("change", calculate);
-
-}
-);
+});
