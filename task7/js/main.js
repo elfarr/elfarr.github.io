d54fdbbb4067bfa0a4b9c2f837c7187f9df7388b
greta novak <114183568+elfarr@users.noreply.github.com>
2023-10-23 11:43:45 +0300

    valid

--- task7/js/main.js
+++ task7/js/main.js
@@ -1,23 +1,23 @@
-
 $(document).ready(function() {
-$('.single-items').slick({
-    infinite: true,
-    dots: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    prevArrow: "<img src='img/l.png' class='prev' style='width: 50px; ' alt='1'>",
-    nextArrow: "<img src='img/r.png' class='next' style='width: 50px'  alt='2'>",
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: true
+  $(".single-items").slick({
+      dots: true,
+      infinite: true,
+      nextArrow: "<img src='img/r.png' class='next'"+
+          "style='width: 50px'  alt='2'>",
+      prevArrow: "<img src='img/l.png' class='prev'"+
+          "style='width: 50px; ' alt='1'>",
+       responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            dots: true,
+            infinite: true,
+            slidesToScroll: 1,
+            slidesToShow: 1
+          },
+      slidesToScroll: 1,
+      slidesToShow: 3
         }
-      }
-    ]
+      ]
+    });
   });
-
-})
