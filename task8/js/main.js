a3cd95df5afd5b0ab74b3579c0a9c8d76af7555e
greta novak <114183568+elfarr@users.noreply.github.com>
2023-11-11 17:26:18 +0300

    task 8

--- task8/js/main.js
+++ task8/js/main.js
@@ -1,23 +1,50 @@
-$(document).ready(function() {
-  $(".single-items").slick({
-      dots: true,
-      infinite: true,
-      nextArrow: "<img src='img/r.png' class='next'"+
-          "style='width: 50px'  alt='2'>",
-      prevArrow: "<img src='img/l.png' class='prev'"+
-          "style='width: 50px; ' alt='1'>",
-      responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            dots: true,
-            infinite: true,
-            slidesToScroll: 1,
-            slidesToShow: 1
-          }
-        }
-      ],
-      slidesToScroll: 1,
-      slidesToShow: 3
-    });
-  });
+document.addEventListener("DOMContentLoaded", function () {
+  window.location.href= '#';
+  const overlay = document.getElementById("overlay");
+  const open = document.getElementById("open");
+  const close = document.getElementById("close");
+  const box = document.getElementById("box");
+  const form =  document.getElementById("form");
+  const send = document.getElementById("send");
+  const successMessage = document.getElementById("successMessage");
+  const dataFromForm = {
+  };
+  close.addEventListener("click", function(event) { 
+    history.back();
+  })
+  open.addEventListener("click", function(event) { 
+    history.pushState({ page: "popup1" }, "Форма", "#popup1"); 
+  })
+  form.addEventListener("submit", function (event) {
+    event.preventDefault(); 
+    const dataFromForm = {
+      name: document.getElementById("fio").value,
+      email: document.getElementById("mail").value,
+      phone: document.getElementById("phone").value,
+      organization: document.getElementById("org").value,
+      message: document.getElementById("text").value,
+      agree: document.getElementById("agree").checked,
+  };
+    var slapform = new Slapform();
+    slapform.submit({
+        form: 'Yesuhuwqb',
+        data: dataFromForm,
+    })
+        .then(function (response) {
+
+            console.log('Success', response)
+        })
+        .catch(function (e) {
+            console.error('Fail', e)
+        })
+        
+  localStorage.setItem("dataFromForm", JSON.stringify(dataFromForm));
+  setTimeout(function () {
+    history.back();
+    successMessage.style.display = "block";
+    box.style.display = "none";
+    form.reset();
+    localStorage.removeItem("dataFromForm");
+  }, 2000);
+
+      }); });
