57e075be61320de829d3da09e48ea599c3983d47
greta novak <114183568+elfarr@users.noreply.github.com>
2023-11-11 18:15:23 +0300

    valid

--- task8/js/main.js
+++ task8/js/main.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-  window.location.href= '#';
+  window.location.href= "#";
   const overlay = document.getElementById("overlay");
   const open = document.getElementById("open");
   const close = document.getElementById("close");
@@ -9,35 +9,35 @@ document.addEventListener("DOMContentLoaded", function () {
   const successMessage = document.getElementById("successMessage");
   const dataFromForm = {
   };
-  close.addEventListener("click", function(event) { 
+  close.addEventListener("click", function(event) {
     history.back();
-  })
-  open.addEventListener("click", function(event) { 
-    history.pushState({ page: "popup1" }, "Форма", "#popup1"); 
-  })
+  });
+  open.addEventListener("click", function(event) {
+    history.pushState({ page: "popup1" }, "Форма", "#popup1");
+  });
   form.addEventListener("submit", function (event) {
-    event.preventDefault(); 
-    const dataFromForm = {
-      name: document.getElementById("fio").value,
-      email: document.getElementById("mail").value,
-      phone: document.getElementById("phone").value,
-      organization: document.getElementById("org").value,
-      message: document.getElementById("text").value,
+    event.preventDefault();
+    dataFromForm = {
       agree: document.getElementById("agree").checked,
+      email: document.getElementById("mail").value,
+      message: document.getElementById("text").value,
+      name: document.getElementById("fio").value,
+      organization: document.getElementById("org").value,
+      phone: document.getElementById("phone").value
   };
-    var slapform = new Slapform();
+    const slapform = new Slapform();
     slapform.submit({
-        form: 'Yesuhuwqb',
         data: dataFromForm,
+        form: "Yesuhuwqb"
     })
         .then(function (response) {
 
-            console.log('Success', response)
+            console.log("Success", response);
         })
         .catch(function (e) {
-            console.error('Fail', e)
-        })
-        
+            alert("Ошибка при отправке формы, перезагрузите страницу");
+            console.error("Fail", e);
+        });
   localStorage.setItem("dataFromForm", JSON.stringify(dataFromForm));
   setTimeout(function () {
     history.back();
